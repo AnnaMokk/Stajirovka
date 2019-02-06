fc445939429d9154d971b3f55f2bfbf9e4f48509
AnnaMokk <vera-makarova-97@mail.ru>
2019-02-06 05:52:45 +0500

    Программа была выполнена примерно на процентов 70%, слеплена из недознаний по C#, В базу данных позитивненько вливаются данные о дате и сумме платежей, ах да, там еще и интерфейс есть , слепленный заботливыми ручками)

--- StajirovkaZadanie/StajirovkaZadanie/Form1.Designer.cs
+++ StajirovkaZadanie/StajirovkaZadanie/Form1.Designer.cs
@@ -28,47 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button2.Location = new System.Drawing.Point(222, 287);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(238, 56);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Добавить в базу данных";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(432, 170);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(68, 17);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Активен";
-            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(12, 30);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(222, 30);
+            this.textBox2.Location = new System.Drawing.Point(540, 89);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(184, 20);
             this.textBox2.TabIndex = 4;
@@ -82,53 +66,112 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(461, 211);
+            this.button3.Location = new System.Drawing.Point(461, 193);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 19);
+            this.button3.Size = new System.Drawing.Size(155, 38);
             this.button3.TabIndex = 6;
-            this.button3.Text = "найти файл";
+            this.button3.Text = "Найти и добавить, если активен";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(432, 30);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(176, 20);
-            this.textBox4.TabIndex = 7;
-            // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(631, 31);
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox5.Location = new System.Drawing.Point(12, 4);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(134, 20);
             this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Дата оплаты";
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Maroon;
-            this.button1.Location = new System.Drawing.Point(680, 371);
+            this.button1.Location = new System.Drawing.Point(593, 285);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(110, 58);
             this.button1.TabIndex = 9;
             this.button1.Text = "Сформировать отчет(Excel)";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox4.Location = new System.Drawing.Point(187, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Информация";
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox6.Location = new System.Drawing.Point(461, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(134, 20);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "Сумма";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(461, 30);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(143, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(187, 30);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(268, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox9.Location = new System.Drawing.Point(48, 211);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(87, 20);
+            this.textBox9.TabIndex = 15;
+            this.textBox9.Text = "Путь к файлу:";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(488, 89);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(46, 20);
+            this.textBox10.TabIndex = 16;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.textBox11.Location = new System.Drawing.Point(362, 89);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(120, 20);
+            this.textBox11.TabIndex = 17;
+            this.textBox11.Text = "Активность:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(748, 415);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button2);
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -136,15 +179,19 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
     }
 }
 
--- StajirovkaZadanie/StajirovkaZadanie/Form1.cs
+++ StajirovkaZadanie/StajirovkaZadanie/Form1.cs
@@ -18,36 +18,137 @@ namespace StajirovkaZadanie
         public Form1()
         {
             InitializeComponent();
-            
         }
-
+        int j = 1;
+        int i = 0;
+        string innn;
         private void button3_Click(object sender, EventArgs e)
         {
+            textBox10.Text = "  ";
+            textBox8.Text = "  ";
+            textBox7.Text = "  ";
+            textBox2.Text = "  ";
+            int k = 0;
+            innn = "";
+            string inf = "";
             if (textBox3.Text != "")
             {
-                OleDbConnection connection;
-                OleDbDataAdapter adapter;
-                DataTable table;
-                BindingSource bindingSource;
-                InitializeComponent();
-                OleDbConnectionStringBuilder stringBuilder = new OleDbConnectionStringBuilder();
-                stringBuilder.DataSource = textBox3.Text;
-                stringBuilder.Provider = "Microsoft.ACE.OLEDB.12.0";
-                stringBuilder.Add("Extended Properties", "Excel 12.0 Xml;HDR=YES");
-                connection = new OleDbConnection();
-                connection.ConnectionString = stringBuilder.ConnectionString;
-                adapter = new OleDbDataAdapter();
-                OleDbCommand command = new OleDbCommand("SELECT * FROM [Лист1$]");
-                command.Connection = connection;
-                adapter.SelectCommand = command;
-                table = new DataTable();
-                adapter.Fill(table);
-                bindingSource = new BindingSource();
-                bindingSource.DataSource = table;
-                textBox1.DataBindings.Add("Text", bindingSource, "Дата оплаты");
-                textBox2.DataBindings.Add("Text", bindingSource, "Сумма");
-                textBox4.DataBindings.Add("Text", bindingSource, "Информация");
+                textBox1.Text = " ";
+                Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
+                Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@textBox3.Text, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing); //открыть файл
+
+                Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1 лист
+                var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//1 ячейку
+
+                Excel.Application ObjWorkExcelNew = new Excel.Application();// создал книгу, можно с ней работать
+                Excel.Workbook workBook = ObjWorkExcelNew.Workbooks.Add();
+                Excel.Worksheet workSheet = workBook.ActiveSheet;
+                string[,] list = new string[3, 10]; // массив значений с листа равен по размеру листу
+                for (i = 0; i < 3; i++)
+                { list[i, j] = ObjWorkSheet.Cells[j + 1, i + 1].Text.ToString(); }//считываем текст в строку//
+
+                textBox1.Text += "     " + list[0, j];
+                textBox8.Text += "     " + list[1, j];
+                textBox7.Text += "     " + list[2, j];
+                inf += textBox8.Text;
+                int len = inf.Length;
+                char[] z = inf.ToCharArray();
+                for (int n = 0; n < len; n++)
+                { if (k != 12)
+                    {
+                        if ((z[n] >= '0') && (z[n] <= '9'))
+                        {
+                            k++;
+                            innn += z[n];
+                        }
+                        else
+                        {
+                            innn = "";
+                            k = 0;
+                        }
+                    }
+
+                }
+
+                if (k == 12) textBox2.Text += innn;
+                else textBox2.Text += " mmmmm " + k;
+                innn = "";
+                k = 0;
+
+
+
+                ObjWorkBook.Close(false, Type.Missing, Type.Missing);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(ObjWorkBook);
+                System.Runtime.InteropServices.Marshal.FinalReleaseComObject(ObjWorkExcel);
+
+                ObjWorkBook = null;
+                ObjWorkExcel = null;
             }
+            j++;
+         ///////////////////////////работа с ACCESS////////////////////////////////////////
+         
+            object ACName = "";
+            object ACINN = "";
+            object ACBOOL = "";
+            object ACID = "";
+
+           
+                for (int i = 1; i <= 4; i++) /////Цикл проверки////////
+                {
+
+                    string ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\Пользователь\\Documents\\GitHub\\Stajirovka\\база.mdb";
+                    OleDbConnection con = new OleDbConnection(ConnectionString);
+                    con.Open();
+
+                    OleDbCommand outt = new OleDbCommand("SELECT [id],[name],[inn],[active] FROM users  Where [id]="+i, con);
+                    OleDbDataReader rdr = outt.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        ACName = rdr["name"];
+                        ACINN = rdr["inn"];
+                        ACBOOL = rdr["active"];
+                    }
+
+                    rdr.Close();
+                    con.Close();
+
+                    string GoalID = ACID.ToString();
+                    string GoalN = ACName.ToString();
+                    string GoalI = ACINN.ToString();
+                    string GoalB = ACBOOL.ToString();
+                    
+
+                    if (GoalB == "Да")
+                        {
+                        textBox10.Text = "Да";
+                            OleDbConnection con1 = new OleDbConnection(ConnectionString);
+                            con1.Open();
+                            string comandString = "INSERT INTO payment ([id_user],[sum],[date])VALUES('"+GoalID+"', '"+textBox7.Text.ToString()+"', '"+textBox1.Text.ToString()+"')";
+                            OleDbCommand addID = new OleDbCommand(comandString, con1);
+                            addID.ExecuteNonQuery();
+                            con1.Close();
+                        }
+                        else
+                        {
+                        }
+                        break;
+                    }
+
+                
+            }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
-}
+
+
+    }
+
+
+
+//C:\Users\Пользователь\Documents\GitHub\Stajirovka\оплаты.xlsx   C:\Users\Пользователь\Documents\GitHub\Stajirovka\база.mdb//
